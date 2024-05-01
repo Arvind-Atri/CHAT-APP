47260b46235c2e97a37db66706f1b4f67a119874
Arvind-Atri <148221394+Arvind-Atri@users.noreply.github.com>
2024-05-01 08:11:39 +0530

    Update socket.js

--- socket/socket.js
+++ socket/socket.js
@@ -5,8 +5,8 @@ const {Server} =require("socket.io");
 const http =require("http");
 const express=require("express")
 const cors=require("cors")
-app.use(cors);
 const app = express();
+app.use(cors);
 
 const server = http.createServer(app);
 const io = new Server(server, {
